6777559123fee70b6743dea1857e0594d68411d7
clarasb04 <clara.serra04@estudiant.upf.edu>
2023-05-10 08:50:09 +0200

    extenció funcions principals

--- estructures.h
+++ estructures.h
@@ -5,21 +5,34 @@
 #ifndef EDAII_ESTRUCTURES_H
 #define EDAII_ESTRUCTURES_H
 #define MAX_LENGHT 30
-
+#define GUSTOS 5
 #endif //EDAII_ESTRUCTURES_H
+#include <stdlib.h>
 
-typedef struct Usuari{
+
+
+typedef struct User{
     char sobrenom[MAX_LENGHT];
     char name[MAX_LENGHT];
     char surname[MAX_LENGHT];
     char gmail[MAX_LENGHT];
     char poblacio[MAX_LENGHT];
-    char sexe[1];
+    int sexe;
     int edat;
-    struct Gustos;
+    char gustos[GUSTOS][MAX_LENGHT];
 };
 
-typedef  struct Gustos{
-    char sexe[2];
-    int edat;
+typedef struct list_nodes{
+    struct User* user;
+    struct list_nodes* next;
+    struct list_nodes* last;
+}list_nodes;
+
+typedef struct llista_usuaris{
+    int size;
+    list_nodes* first;
+    list_nodes* last;
 };
+
+
+
--- main.h
+++ main.h
@@ -11,3 +11,6 @@ void all_users();
 int printmenu();
 void new_user();
 void interactuar();
+void enviar_s_amistat();
+void publicacio();
+void gestionar_s_pendents();
